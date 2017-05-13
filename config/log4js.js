a3a8380968f3a98745d2a7f69b389a647c06071d
lyf <1601942611@qq.com>
2017-05-13 17:25:35 +0800

    test

--- config/log4js.js
+++ config/log4js.js
@@ -27,10 +27,11 @@ const theAbsolutePathDirOfLogfiles = path.resolve(__dirname, '../', 'logs')
 const pattern = "[%d] [%p] - %m"
 const log4jsConfig = {
   appenders: [
+    // {
+    //   type: "console",
+    //   category: "console"
+    // }, 
     {
-      type: "console",
-      category: "console"
-    }, {
       //
       category: "http",
       type: "file",
@@ -63,12 +64,12 @@ const log4jsConfig = {
       }
     }
   ],
-  replaceConsole: true,
+  // replaceConsole: true,
   levels: {
     business: "info",
     exception: "ALL",
-    http: "ALL",
-    console: "ALL"
+    http: "ALL"
+    // , console: "ALL"
   }
 }
 
