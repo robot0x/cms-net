3fa17db698c954b443725d0327ee17c63362af43
lyf <1601942611@qq.com>
2017-06-02 18:45:32 +0800

    fix url拼错的bug

--- src/render/mobile/author/parser.js
+++ src/render/mobile/author/parser.js
@@ -15,7 +15,7 @@ class AuthorParser {
     }
     for (let meta of metas) {
       html += `
-        <div class="articlecard" data-href="//c.diax2.com/view/app/?m=show&id=${meta.nid}">
+        <div class="articlecard" data-href="//c.diaox2.com/view/app/?m=show&id=${meta.nid}">
           <img class="articleimg" src="${meta.thumb_image_url}"/>
           <span class="articletitle">${meta.title.join('')}</span>
           <span class="articleread" data-id="${4294967297 * meta.nid}">阅读 ...</span>
