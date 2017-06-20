c8706a0bb6d078b14332d673ba5d99afc5e965f5
lyf <1601942611@qq.com>
2017-06-20 20:52:21 +0800

    metas接口格式修改

--- src/api/meta.js
+++ src/api/meta.js
@@ -1,10 +1,15 @@
 const MetaService = require('../service/MetaService')
 const metaService = new MetaService()
 const Log = require('../utils/Log')
+const _ = require('lodash')
 
-function getMetas (ids) {
+async function getMetas (ids) {
   Log.business('[API meta] 输入参数为：', ids)
-  return metaService.getRawMetas(ids)
+  let metas = (await metaService.getRawMetas(ids)) || []
+  if (_.isPlainObject(metas)) {
+    metas = [metas]
+  }
+  return { metas }
 }
 
 module.exports = getMetas
