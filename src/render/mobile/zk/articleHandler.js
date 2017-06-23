a07f62403acb014d8b8e24d267af2f514ce5fea5
lyf <1601942611@qq.com>
2017-06-23 12:11:06 +0800

    fix 专刊引用的文章，如果是首页，使用coverex代替cover的策略实现错误

--- src/render/mobile/zk/articleHandler.js
+++ src/render/mobile/zk/articleHandler.js
@@ -23,7 +23,12 @@ module.exports = async (html, ids) => {
   const container = $('#container')
   try {
     const articleDoms = Array.from(container.find('.goodthing'))
-    const metas = await metaService.getRawMetas(ids, false, true)
+    const metas = await metaService.getRawMetas(
+      ids,
+      false, // useBuylink
+      true, // isShortId
+      true // useCoverex
+    )
     for (let articleDom of articleDoms) {
       // console.log(articleDom.attribs['data-href'])
       let meta = findMetaById(
@@ -35,16 +40,21 @@ module.exports = async (html, ids) => {
         $(articleDom).remove()
         continue
       }
+      let {ctype, title, cover_image_url, coverex_image_url} = meta
       const $articleDom = $(articleDom)
       let titleDom = $articleDom.find('.title')
-      let title = (meta.title[0] || '').replace(/ {2}/, '<br>')
+      title = (title[0] || '').replace(/ {2}/, '<br>')
       if (titleDom[0]) {
         titleDom.text(title)
       } else {
         $articleDom.find('.mask-title').text(title)
       }
+      // 渲染策略：如果是首页，取coverex作为渲染的图，其他的都是取cover
+      if (ctype === 1) {
+        cover_image_url = coverex_image_url　// eslint-disable-line
+      }
       // 加上@768_1l 减小图片大小，经测试专刊页可以减少至少10%的下载量
-      $articleDom.find('.direct').attr('src', Utils.addProtocolHead(Utils.addImageOfShowPageAliImageSuffix(meta.cover_image_url)))
+      $articleDom.find('.direct').attr('src', Utils.addProtocolHead(Utils.addImageOfShowPageAliImageSuffix(cover_image_url)))
     }
   } catch (error) {
     console.log(error)
--- src/render/mobile/zk/index.js
+++ src/render/mobile/zk/index.js
@@ -47,7 +47,7 @@ class ZKRender extends Render {
       content = content || ''
       meta = meta || {title: '', titleex: ''}
       images = images || []
-      let { title, titleex, ctype } = meta
+      let { title, titleex } = meta
       parser.markdown = content // markdown is a setter like method `setMarkdown`
       //  对于专刊，我们要先取出所引用的所有文章id
       let data = (Utils.getZkDataByParseMarkdown(content)) || {}
@@ -77,26 +77,13 @@ class ZKRender extends Render {
       //   // let buylink = await metaService.getBuylink(cid)
       //   buylinks.push({ cid, link: Utils.convertSkuUrl(buylink, cid) })
       // }
-
-      // console.log(`ID为${id}的专刊引用的文章ID列表为：`,cids)
-      // body = imageHandler(body, images)
       //  0未设置类型,没有被使用/第1位-内容图(1)/第2位cover图(2)/第3位coverex图(4)/第4位thumb图(8)/第5位swipe图(16)/第6位banner图(32)
       let cover = images.filter(img => {
         return (img.type & 2) === 2
       })
-      let coverex = images.filter(img => {
-        return (img.type & 8) === 4
-      })
       let thumb = images.filter(img => {
         return (img.type & 8) === 8
       })
-      // 渲染策略：如果是首页，取coverex作为渲染的图，其他的都是取cover
-      if (ctype === 1) {
-        cover = coverex
-      }
-      // const swipes = images.filter(img => {
-      //   return (img.type & 16) === 16
-      // }) || []
       thumb = Utils.getFirst(thumb) || {}
       cover = Utils.getFirst(cover) || {}
       let body = parser
