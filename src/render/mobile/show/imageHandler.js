46a24acd71f2431da767412278cf28780faf8301
lyf <1601942611@qq.com>
2017-07-05 15:20:27 +0800

    sku图片改为https； 图片如果不再图片库中存在，也要赋予其基本的属性，包括src/data-src/data-big；

--- src/render/mobile/show/imageHandler.js
+++ src/render/mobile/show/imageHandler.js
@@ -25,39 +25,38 @@ var findByUrl = (url, images) => {
  * 所以导致 findByUrl 找不到结果，导致图片没有加上width和height属性，导致图片超出页面范围
  */
 module.exports = (html, images, usePlaceholder = true) => {
-  const $ = cheerio.load(`<div id="container">${html}</div>`, { decodeEntities: false })
+  const $ = cheerio.load(`<div id="container">${html}</div>`, {
+    decodeEntities: false
+  })
   const container = $('#container')
   const imgs = Array.from(container.find('img'))
   for (let img of imgs) {
     let image = findByUrl(Utils.removeProtocolHead(img.attribs.src), images)
     if (image) {
-      let {attribs} = img
+      let { attribs } = img
       if ($(img).hasClass('articleimg')) {
         continue
       }
-      // 在IOS APP 9.x下，由于ATS的限制，非http的图片无法显示
-      let big = Utils.addProtocolHead(image.url)
+      // 如果图片在数据库中存在，则才赋予宽/高/alt属性
       img.attribs.alt = image.alt
       img.attribs.width = attribs['data-w'] = image.width
       img.attribs.height = attribs['data-h'] = image.height
-      if (usePlaceholder) {
-        img.attribs.src = Utils.addProtocolHead(placeholder)
-      }
-      img.attribs['data-big'] = big
-      img.attribs['data-src'] = Utils.addImageOfShowPageAliImageSuffix(big)
-      // 不管是否有图，都加上lazy，因为lazy有样式，其中有 width:100%
-      // 如果我们由于同步不及时或者使用的外部的图片
-      // 则图片过大的话，会超出页面主容器，所以做个兜底
-      $(img).addClass('lazy')
     } else {
       // 就算数据库中没有这张图片，为了防止在ios9一下由于ATS的限制出现图片未加载
       img.attribs.src = Utils.addProtocolHead(img.attribs.src)
-      // 不管是否有图，都加上lazy，因为lazy有样式，其中有 width:100%
-      // 如果我们由于同步不及时或者使用的外部的图片
-      // 则图片过大的话，会超出页面主容器，所以做个兜底
-      $(img).addClass('lazy')
     }
-    
+    // 不管图片是否在数据库里，都要赋予其最基本的属性：src/data-src/data-big/class=lazy
+    // 在IOS APP 9.x下，由于ATS的限制，非http的图片无法显示
+    let big = Utils.addProtocolHead(img.attribs.src)
+    if (usePlaceholder) {
+      img.attribs.src = Utils.addProtocolHead(placeholder)
+    }
+    img.attribs['data-big'] = big
+    img.attribs['data-src'] = Utils.addImageOfShowPageAliImageSuffix(big)
+    // 不管是否有图，都加上lazy，因为lazy有样式，其中有 width:100%
+    // 如果我们由于同步不及时或者使用的外部的图片
+    // 则图片过大的话，会超出页面主容器，所以做个兜底
+    $(img).addClass('lazy')
   }
   return container.html()
 }
