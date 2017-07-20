0bf429562b36946898b108d632aa88c5a9f20e2f
lyf <1601942611@qq.com>
2017-07-20 11:23:07 +0800

    update

--- src/api/relsearch.js
+++ src/api/relsearch.js
@@ -5,10 +5,12 @@ async function relsearch (id) {
   if (!id) return null
   Log.business('[API relsearch] 输入参数为：', id)
   let sql = `SELECT related_words FROM diaodiao_sim_query WHERE aid = ${id}`
-  let relwords = Utils.getFirst(await DB.exec(sql)) || []
+  let relwords = Utils.getFirst(await DB.exec(sql))
   Log.business(`[API relsearch] ${sql}\nfetch data is ${relwords} `)
   if (relwords && relwords.related_words && relwords.related_words.trim()) {
     relwords = relwords.related_words.split(/\s/)
+  } else {
+    relwords = []
   }
   return relwords
 }
