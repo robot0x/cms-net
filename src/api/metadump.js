acb372e4b0aa20b466cfa32f813f254567dbff14
lyf <1601942611@qq.com>
2017-07-20 15:29:12 +0800

    metadump下发title字段

--- src/api/metadump.js
+++ src/api/metadump.js
@@ -2,7 +2,7 @@ const DB = require('../db/DB')
 const Log = require('../utils/Log')
 async function metadump () {
   try {
-    return await DB.exec(`SELECT id,ctype,author,timetopublish AS pubtime FROM diaodiao_article_meta`)
+    return await DB.exec(`SELECT id,ctype,author,timetopublish AS pubtime,title FROM diaodiao_article_meta`)
   } catch (error) {
     Log.exception(error)
   }
