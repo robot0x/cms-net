b2c22ce23de729ad32ff36a9feacc9aec32798de
lyf <1601942611@qq.com>
2017-08-16 12:28:06 +0800

    更新adv接口

--- src/api/adv.js
+++ src/api/adv.js
@@ -1,7 +1,7 @@
 const DB = require('../db/DB')
 async function adv (isDev = false) {
   // insert into cms_adv (type, can_leave, value, image,aid) values ('link',1,'https://c.diaox2.com/view/app/?m=show&id=10633', 'https://content.image.alimmdn.com/adv/1234567890.jpg', 10633);
-  let sql = `SELECT type,can_leave,value,image,aid * 4294967297 AS id, start, end FROM cms_adv `
+  let sql = `SELECT type,can_leave,value,image,aid * 4294967297 AS id, start, end FROM ${isDev ? 'cms_adv_dev' : 'cms_adv'} `
   // 如果是灰度接口，则选出 now < start的数据，即还未上线的数据
   // 否则的话，选出 start <= now <= end 之间的数据
   // let now = new Date()
