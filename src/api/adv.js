d10b96117a41b2420d42be930205df3910fd8efa
lyf <1601942611@qq.com>
2017-08-16 16:58:26 +0800

    更新广告接口

--- src/api/adv.js
+++ src/api/adv.js
@@ -1,16 +1,7 @@
 const DB = require('../db/DB')
 async function adv (isDev = false) {
-  // insert into cms_adv (type, can_leave, value, image,aid) values ('link',1,'https://c.diaox2.com/view/app/?m=show&id=10633', 'https://content.image.alimmdn.com/adv/1234567890.jpg', 10633);
-  let sql = `SELECT type,can_leave,value,image,aid * 4294967297 AS id, start, end FROM ${isDev ? 'cms_adv_dev' : 'cms_adv'} `
-  // 如果是灰度接口，则选出 now < start的数据，即还未上线的数据
-  // 否则的话，选出 start <= now <= end 之间的数据
-  // let now = new Date()
-  // let escapeNow = DB.escape(now)
-  // let where = `where ${escapeNow} >= start AND ${escapeNow} <= end`
-  // if (isDev) {
-  //   where = `where ${escapeNow} < start`
-  // }
-  // sql += where
+  // 选出当前正在进行的广告和未来要上线的广告，即满足 now < end 条件的数据
+  let sql = `SELECT type,can_leave,value,image,aid * 4294967297 AS id, start, end FROM ${isDev ? 'cms_adv_dev' : 'cms_adv'} where ${DB.escape(new Date())} < end`
   let advs = (await DB.exec(sql)) || []
   return advs.map(a => {
     a.start = Math.floor(new Date(a.start).getTime() / 1000)
